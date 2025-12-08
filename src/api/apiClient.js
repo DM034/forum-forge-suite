37788162610f3458ff908a5a49da5ac357f19564
DM034 <michaeldm034@gmail.com>
2025-12-08 13:49:48 +0300

    Update apiClient.js

--- src/api/apiClient.js
+++ src/api/apiClient.js
@@ -1,9 +1,14 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: "https://forum-backend-ea7r.onrender.com/api", // 👈 on force
   timeout: 15000
 });
+
+// const apiClient = axios.create({
+//   baseURL: import.meta.env.VITE_API_URL,
+//   timeout: 15000
+// });
 
 apiClient.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
