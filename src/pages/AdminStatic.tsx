8b5ac4571af4c52dfdaa74cb30984f392dc1748a
DM <michaeldm034@gmail.com>
2026-01-13 01:33:28 +0300

    Update AdminStatic.tsx

--- src/pages/AdminStatic.tsx
+++ src/pages/AdminStatic.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Layout from "@/components/Layout";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
